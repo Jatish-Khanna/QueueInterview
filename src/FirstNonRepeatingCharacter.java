33381e9dc01a9ba3c6fdd1c5d2194bb01e6ed819
Jatish Khanna <jatish.khanna@gmail.com>
2025-01-21 06:38:13 -0700

    First Non repeating character implementation

--- src/FirstNonRepeatingCharacter.java
+++ src/FirstNonRepeatingCharacter.java
@@ -1,4 +1,27 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class FirstNonRepeatingCharacter {
+
+  int[] data;
+  Queue<Character> dataQueue;
+
+  FirstNonRepeatingCharacter() {
+    data = new int[256];
+    dataQueue = new LinkedList<>();
+  }
+
+  private char getFirstNonRepeating() {
+    while (!dataQueue.isEmpty() && data[dataQueue.peek()] > 1) {
+      dataQueue.poll();
+    }
+    return dataQueue.isEmpty() ? '#' : dataQueue.peek();
+  }
+
+  private void insert(char newChar) {
+    dataQueue.offer(newChar);
+    data[newChar]++;
+  }
 
   public static void main(String[] args) {
     FirstNonRepeatingCharacter stream = new FirstNonRepeatingCharacter();
@@ -7,6 +30,11 @@ public class FirstNonRepeatingCharacter {
     stream.insert('a');
     System.out.println(stream.getFirstNonRepeating()); // Output: 'b'
     stream.insert('c');
+    System.out.println(stream.getFirstNonRepeating()); // Output: 'b'
+    stream.insert('b');
+    stream.insert('a');
+    System.out.println(stream.getFirstNonRepeating()); // Output: 'b'
+    stream.insert('d');
     System.out.println(stream.getFirstNonRepeating()); // Output: 'b'
   }
 }
