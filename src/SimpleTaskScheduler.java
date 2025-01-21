ba41916518a30a448f6b9e1aaf2f6a81098191be
Jatish Khanna <jatish.khanna@gmail.com>
2025-01-21 07:55:32 -0700

    Simple task scheduler with rolling sum

---
+++ src/SimpleTaskScheduler.java
@@ -0,0 +1,11 @@
+public class SimpleTaskScheduler {
+
+  // Main function to execute the program
+  public static void main(String[] args) {
+    // Example task durations
+    int[] tasks = {4, 2, 5, 3};
+
+    // Process and print the results
+    processTasks(tasks);
+  }
+}
