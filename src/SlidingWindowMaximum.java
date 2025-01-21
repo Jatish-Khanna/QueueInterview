077e4f9197e3a4d690569025ab750854053632ff
Jatish Khanna <jatish.khanna@gmail.com>
2025-01-21 06:56:32 -0700

    Sliding window maximum - Implementation

--- src/SlidingWindowMaximum.java
+++ src/SlidingWindowMaximum.java
@@ -1,6 +1,41 @@
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class SlidingWindowMaximum {
+
+  static LinkedList<Integer> window = new LinkedList<>();
+
+  private static int[] maxSlidingWindow(int[] nums, int k) {
+    if (k <= 1) {
+      return nums;
+    }
+    int index = 0;
+    while (index < k) {
+      while (!window.isEmpty() && nums[window.peek()] < nums[index]) {
+        window.pollLast();
+      }
+      window.offer(index);
+      ++index;
+    }
+    int[] result = new int[nums.length - k + 1];
+    while (index < nums.length) {
+      result[index - k] = nums[window.peek()];
+
+      if (window.peek() <= index - k) {
+        window.poll();
+      }
+
+      while (!window.isEmpty() && nums[window.peek()] < nums[index]) {
+        window.pollLast();
+      }
+      window.offer(index);
+      ++index;
+    }
+    result[index - k] = nums[window.peek()];
+
+    return result;
+  }
 
   public static void main(String[] args) {
     int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
